c66c28ce1bdabd35bb45018eb2d3170fb30f2bb8
TaiVanNgo <vantaingo.056@gmail.com>
2024-07-26 19:38:34 +0700

    update new crypto solving challenges

--- Cryptopals/set1/challenge03/solution.py
+++ Cryptopals/set1/challenge03/solution.py
@@ -6,15 +6,41 @@ def bxor(message, key):
     # convert to byte again and print
     return bytes(cipher_text)
 # -----------------------------------------------------------------------------
+ascii_text_list = list(range(97, 122)) + [32] #An array of ascii value from a-z (including the space " ")
 
+def attack_single_byte_xor(cipher_text):
+    # A dictionary to keep track the best candidate
+    best = None
 
-#origin hex string
+    #since we do with a byte -> a byte consists of 8 bits, so we find all the possible key from
+    # 0 -> 255  
+    for i in range (2**8):
+        #convert the candidate key from a number to byte
+        candidate_key = i.to_bytes(1, byteorder='big')
+        key_stream = candidate_key * len(cipher_text) #make a candidate_key have the same length with the encrypted text
+        candidate_message = bxor(cipher_text, key_stream)
+        number_of_letters = sum([x in ascii_text_list for x in candidate_message])
+
+        #if the candidate key (possible key) has more valid letter than the other candidate before
+        #we store it to best
+        if best == None or number_of_letters > best['number_of_valid_letters']:
+            #store the best key 
+            best = {
+                "message": candidate_message,
+                'key': candidate_key,
+                "number_of_valid_letters": number_of_letters
+            }
+    
+    return best
+
 hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-# convert to bytes
 hex_bytes = bytes.fromhex(hex_string)
 
-possible_key = bytes([1])
-print(possible_key)
-key_stream = possible_key * len(hex_bytes) #Repeat the key so it has the same length with the message
+result = attack_single_byte_xor(hex_bytes)
 
-bxor(hex_bytes, key_stream)
+print(result)
+# print('key:', result['key'])
+
+
+
+
