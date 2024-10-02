6a791d66ee4a6c834b67906d1a53618ce70924da
TaiVanNgo <vantaingo.056@gmail.com>
2024-10-02 17:51:27 +0700

    update crytopal

--- Cryptopals/set1/challenge02/solution.py
+++ Cryptopals/set1/challenge02/solution.py
@@ -12,18 +12,16 @@ print(f"The hex string number 2 in raw bytes: {bytes_2}\n")
 bytes_list = []
 
 for i in range(len(bytes_1)):
-    bytes_list.append(bytes_1[i] ^ bytes_2[i]) # perform XOR operation on each byte
+    # perform XOR operation on each byte
+    bytes_list.append(bytes_1[i] ^ bytes_2[i])
     # the XOR operation return a ascii value (0 - 255)
-# the bytes_list after the for loop will be an array of decimal, the decimal here is 
+# the bytes_list after the for loop will be an array of decimal, the decimal here is
 # the ascii value in the value after them xor with each other
 print(f"The list of ascii value after XOR: {bytes_list}")
 
 # convert it to raw bytes
 bytes_result = bytes(bytes_list)
-print("The result in raw bytes: ",bytes_result) 
+print("The result in raw bytes: ", bytes_result)
 
-#Convert it back to hexadecimal
+# Convert it back to hexadecimal
 print("The final result: ", hexlify(bytes_result).decode())
-
-
-
--- Cryptopals/set1/challenge03/solution.py
+++ Cryptopals/set1/challenge03/solution.py
@@ -1,37 +1,44 @@
 def bxor(message, key):
-    cipher_text = bytearray() # define a bytearray
+    cipher_text = bytearray()  # define a bytearray
     for i in range(len(message)):
-        cipher_text.append(message[i] ^ key[i]) # append to the array
+        cipher_text.append(message[i] ^ key[i])  # append to the array
 
-    # convert to byte again and print
+    # convert to byte again
     return bytes(cipher_text)
+
+
 # -----------------------------------------------------------------------------
-ascii_text_list = list(range(97, 122)) + [32] #An array of ascii value from a-z (including the space " ")
+# An array of ascii value from a-z (including the space " ")
+ascii_text_list = list(range(97, 122)) + [32]
+
 
 def attack_single_byte_xor(cipher_text):
     # A dictionary to keep track the best candidate
     best = None
 
-    #since we do with a byte -> a byte consists of 8 bits, so we find all the possible key from
-    # 0 -> 255  
-    for i in range (2**8):
-        #convert the candidate key from a number to byte
-        candidate_key = i.to_bytes(1, byteorder='big')
-        key_stream = candidate_key * len(cipher_text) #make a candidate_key have the same length with the encrypted text
+    # since we do with a byte -> a byte consists of 8 bits, so we find all the possible key from
+    # 0 -> 255
+    for i in range(2**8):
+        # convert the candidate key from a number to byte
+        candidate_key = i.to_bytes(1, byteorder='big')  # from (0x00 to 0xff)
+        # make a candidate_key have the same length with the encrypted text
+        key_stream = candidate_key * len(cipher_text)
         candidate_message = bxor(cipher_text, key_stream)
-        number_of_letters = sum([x in ascii_text_list for x in candidate_message])
+        number_of_letters = sum(
+            [x in ascii_text_list for x in candidate_message])
 
-        #if the candidate key (possible key) has more valid letter than the other candidate before
-        #we store it to best
+        # if the candidate key (possible key) has more valid letter than the other candidate before
+        # we store it to best
         if best == None or number_of_letters > best['number_of_valid_letters']:
-            #store the best key 
+            # store the best key
             best = {
                 "message": candidate_message,
                 'key': candidate_key,
                 "number_of_valid_letters": number_of_letters
             }
-    
+
     return best
+
 
 hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 hex_bytes = bytes.fromhex(hex_string)
@@ -39,8 +46,3 @@ hex_bytes = bytes.fromhex(hex_string)
 result = attack_single_byte_xor(hex_bytes)
 
 print(result)
-# print('key:', result['key'])
-
-
-
-
